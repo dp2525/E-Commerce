5e24173fb64bdbefd335ea89b630fd17b4f87eda
Nidhi Patel <pnidhig@gmail.com>
2023-08-04 15:23:13 -0400

    modify home page

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
-let slideIndex = 1;
-showSlides();
+debugger;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+$('.slider').bxSlider({
+  auto: true
+});
 
-function showSlides(n) {
-  // debugger;
-  let slides = $(".mySlides");
 
-  let dots = $(".dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  slides.hide();
-  dots.removeClass("active");
-  slides.eq(slideIndex - 1).show();
-  dots.eq(slideIndex - 1).addClass("active");
-}
+// let slideIndex = 1;
+// showSlides();
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+//   // debugger;
+//   let slides = $(".mySlides");
+
+//   let dots = $(".dot");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   slides.hide();
+//   dots.removeClass("active");
+//   slides.eq(slideIndex - 1).show();
+//   dots.eq(slideIndex - 1).addClass("active");
+// }
